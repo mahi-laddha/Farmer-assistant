94c8be1c88606e70baed7ef1b659f9b772a703b6
unknown <mahi laddha>
2026-04-17 18:22:01 +0530

    Fix import path in auth module (fixes #1)

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { registerUser, loginUser } from "./apis";
+import { registerUser, loginUser } from "./api";
 
 const AuthContext = createContext();
 
